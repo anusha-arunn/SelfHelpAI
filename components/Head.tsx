406df5cb6750ec196ce24f2922515f9bfadc1eac
anusha-arunn <anusha.arunn@gmail.com>
2023-01-16 14:28:29 +0530

    Rectified

---
+++ components/Head.tsx
@@ -0,0 +1,85 @@
+import Head from "next/head";
+
+export default function Main() {
+  return (
+    <>
+      <Head>
+        {/* SEO */}
+
+        <title>AI Therapist</title>
+
+        <meta name="description" content="Quick solutions for your website." />
+        <meta name="keywords" content="AI Therapist, Mental Health, WHO" />
+        <meta property="og:url" content="https://aitherapist.co/" />
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta content="IE=edge" httpEquiv="X-UA-Compatible" />
+        <meta name="robots" content="all" />
+
+        {/* Open Graph
+        <meta property="og:title" content="HandCraftWeb" />
+        <meta
+          property="og:description"
+          content="Quick solutions for your website."
+        />
+        <meta property="og:url" content="https://handcraftweb.com/" />
+        <meta property="og:type" content="website" />
+        <meta property="og:locale" content="en_GB" />
+        <meta
+          property="og:image"
+          content="https://handcraftweb.com/og_image/og_image.png"
+        />
+        <meta property="og:image:width" content="1200" />
+        <meta property="og:image:height" content="630" /> */}
+
+        {/* Favicons
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="favicons/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+          href="favicons/favicon-32x32.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+          href="favicons/favicon-16x16.png"
+        />
+        <link rel="manifest" href="favicons/site.webmanifest" />
+        <link
+          rel="mask-icon"
+          href="favicons/safari-pinned-tab.svg"
+          color="#ffffff"
+        />
+        <meta name="msapplication-TileColor" content="#ffffff" /> */}
+
+        {/* Twitter */}
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:site" content="@aitherapist" />
+        <meta name="twitter:creator" content="@aitherapist" />
+        <meta name="twitter:title" content="AI Therapist" />
+        <meta
+          name="twitter:description"
+          content="Quick solutions for your website."
+        />
+
+        {/* Tab color */}
+        <meta
+          name="theme-color"
+          content="#FFFFFF"
+          media="(prefers-color-scheme: light)"
+        />
+        <meta
+          name="theme-color"
+          content="#000000"
+          media="(prefers-color-scheme: dark)"
+        />
+      </Head>
+    </>
+  );
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,4 +1,5 @@
 import Head from "next/head";
+import HeadSEO from "../components/Head";
 import Image from "next/image";
 import Script from "next/script";
 
@@ -6,18 +7,17 @@ export default function Home() {
   return (
     <div className="text-center justify-center">
       <Head>
-        <title>AI Therapist</title>
-        <meta property="og:title" content="AI Therapist" key="title" />
-        <meta name="robots" content="all" />
-        <Script
-          dangerouslySetInnerHTML={{
-            __html: `
+        <HeadSEO />
+      </Head>
+      <Script
+        id="0"
+        dangerouslySetInnerHTML={{
+          __html: `
     window.heap=window.heap||[],heap.load=function(e,t){window.heap.appid=e,window.heap.config=t=t||{};var r=document.createElement("script");r.type="text/javascript",r.async=!0,r.src="https://cdn.heapanalytics.com/js/heap-"+e+".js";var a=document.getElementsByTagName("script")[0];a.parentNode.insertBefore(r,a);for(var n=function(e){return function(){heap.push([e].concat(Array.prototype.slice.call(arguments,0)))}},p=["addEventProperties","addUserProperties","clearEventProperties","identify","resetIdentity","removeEventProperty","setEventProperties","track","unsetEventProperty"],o=0;o<p.length;o++)heap[p[o]]=n(p[o])};
   heap.load("2281307306");
    `,
-          }}
-        />
-      </Head>
+        }}
+      />
 
       <div className="text-center items-center justify-center">
         <p className="text-4xl pt-5">Support Better Mental Health</p>
