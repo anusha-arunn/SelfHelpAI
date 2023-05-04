859bd5f158771652a818d4c7d070dc56a7e4c64c
anusha-arunn <anusha.arunn@gmail.com>
2023-05-04 14:53:13 +0530

    changed paths

--- pages/api/blog.js
+++ pages/api/blog.js
@@ -1,13 +1,13 @@
 import * as fs from "fs";
 
 export default async function handler(req, res) {
-  let data = await fs.promises.readdir("components/Blog");
+  let data = await fs.promises.readdir("Blog");
   console.log(data);
   let myfile;
   let allBlogs = [];
   for (let index = 0; index < data.length; index++) {
     const item = data[index];
-    myfile = await fs.promises.readFile("components/Blog/" + item, "utf-8");
+    myfile = await fs.promises.readFile("Blog/" + item, "utf-8");
     allBlogs.push(JSON.parse(myfile));
   }
   res.status(200).json(allBlogs);
--- pages/api/getblogs.js
+++ pages/api/getblogs.js
@@ -1,15 +1,11 @@
 import * as fs from "fs";
 
 export default function handler(req, res) {
-  fs.readFile(
-    `components/Blog/${req.query.slug}.json`,
-    "utf-8",
-    (err, data) => {
-      if (err) {
-        return res.status(500).json({ error: "No such blog" });
-      }
-      console.log(req.query.slug);
-      return res.status(200).json(JSON.parse(data));
+  fs.readFile(`Blog/${req.query.slug}.json`, "utf-8", (err, data) => {
+    if (err) {
+      return res.status(500).json({ error: "No such blog" });
     }
-  );
+    console.log(req.query.slug);
+    return res.status(200).json(JSON.parse(data));
+  });
 }
