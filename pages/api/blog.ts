d81cf2006ffa6fdfa08f30f7a0d8f6748c675779
anusha-arunn <anusha.arunn@gmail.com>
2023-05-05 11:41:13 +0530

    trying changes to .ts

--- pages/api/blog.ts
+++ pages/api/blog.ts
@@ -5,13 +5,13 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse
 ) {
-  let data = await fs.promises.readdir("Blog");
+  let data = await fs.promises.readdir("components/Blog");
   console.log(data);
   let myfile;
   let allBlogs = [];
   for (let index = 0; index < data.length; index++) {
     const item = data[index];
-    myfile = await fs.promises.readFile("Blog/" + item, "utf-8");
+    myfile = await fs.promises.readFile("components/Blog/" + item, "utf-8");
     allBlogs.push(JSON.parse(myfile));
   }
   res.status(200).json(allBlogs);
