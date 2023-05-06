37e560c97af8353393dcc2026c5796e72f5069c3
anusha-arunn <anusha.arunn@gmail.com>
2023-05-06 17:49:49 +0530

    Working

--- pages/api/getblogs.ts
+++ pages/api/getblogs.ts
@@ -1,12 +1,18 @@
-import * as fs from "fs";
+import fs from "fs";
+import path from "path";
 import { NextApiRequest, NextApiResponse } from "next";
 
 export default function handler(req: NextApiRequest, res: NextApiResponse) {
-  fs.readFile(`Blog/${req.query.slug}.json`, "utf-8", (err, data) => {
+  const blogPostPath = path.join(
+    process.cwd(),
+    "data",
+    "blog",
+    `${req.query.slug}.json`
+  );
+  fs.readFile(blogPostPath, "utf8", (err, data) => {
     if (err) {
       return res.status(500).json({ error: "No such blog" });
     }
-    console.log(req.query.slug);
     return res.status(200).json(JSON.parse(data));
   });
 }
