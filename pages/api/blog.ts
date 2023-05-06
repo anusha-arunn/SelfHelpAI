87fbbe03a77cab490e82db6e27cbe3a9e43bd349
anusha-arunn <anusha.arunn@gmail.com>
2023-05-06 17:36:50 +0530

    Changed path

--- pages/api/blog.ts
+++ pages/api/blog.ts
@@ -6,14 +6,11 @@ export default async function handler(
   res: NextApiResponse
 ) {
   try {
-    const data = await fs.promises.readdir("components/Blog");
+    const data = await fs.promises.readdir("public/Blog");
     console.log(data);
     const allBlogs = [];
     for (const item of data) {
-      const myfile = await fs.promises.readFile(
-        `components/Blog/${item}`,
-        "utf-8"
-      );
+      const myfile = await fs.promises.readFile(`public/Blog/${item}`, "utf-8");
       const blog = JSON.parse(myfile);
       allBlogs.push(blog);
     }
